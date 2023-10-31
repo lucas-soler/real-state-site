832e95ce9a3f00cdb77f79f757f98cde17d08137
Lucas Soler <lucas.p.soler@gmail.com>
2023-10-31 17:24:59 -0300

    UUIDs are now fixed

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,4 @@
 import { Property, PropertyCard } from "@/components/PropertyCard";
-import { v4 as uuidv4 } from "uuid";
 
 function generatePropertySlug(this: Property) {
   return (
@@ -13,11 +12,11 @@ function generateCitySlug(this: Property) {
 
 export const properties: Property[] = [
   {
-    id: uuidv4(),
+    id: "183aff29-0fcb-4e3f-95b3-7ad0413b0265",
     slug: generatePropertySlug,
     pictures: [
       {
-        id: uuidv4(),
+        id: "ac7f696b-4561-4061-b5cf-64060706b72c",
         isMain: true,
         order: 1,
         url: "https://resizedimgs.vivareal.com/fit-in/870x653/named.images.sp/f97a58fe76031c9b324457cb18c2bb8d/%7Bdescription%7D.jpg",
@@ -91,11 +90,11 @@ export const properties: Property[] = [
     email: "lucas.p.soler@gmail.com",
   },
   {
-    id: uuidv4(),
+    id: "d89f1a32-d211-4022-9d64-e02c5c76e82d",
     slug: generatePropertySlug,
     pictures: [
       {
-        id: uuidv4(),
+        id: "f66dd19b-4e57-4de1-87db-ebb8351db0e3",
         isMain: true,
         order: 1,
         url: "https://resizedimgs.vivareal.com/fit-in/870x653/named.images.sp/de4b20c045e1a3c537f3ead9afc28b00/%7Bdescription%7D.jpg",
@@ -155,7 +154,7 @@ export const properties: Property[] = [
     email: "lucas.p.soler@gmail.com",
   },
   {
-    id: uuidv4(),
+    id: "1c46e581-47d6-4ade-90a0-b1bc70866a05",
     slug: generatePropertySlug,
     pictures: [],
     description: "4 beds, 3 baths apartment for sale, 101 m²",
