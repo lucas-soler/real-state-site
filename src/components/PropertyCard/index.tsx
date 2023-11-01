948743f5e781d195caf082827663923546b9741d
Lucas Soler <lucas.p.soler@gmail.com>
2023-11-01 15:53:11 -0300

    border change

--- src/components/PropertyCard/index.tsx
+++ src/components/PropertyCard/index.tsx
@@ -72,7 +72,7 @@ export function PropertyCard({
     "flex flex-1 flex-col border border-regal-blue rounded-sm";
 
   if (isSlide) {
-    containerStyle = `keen-slider__slide number-slide${order}`;
+    containerStyle = `border border-regal-blue rounded-sm keen-slider__slide number-slide${order}`;
   }
 
   return (
