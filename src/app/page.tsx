b39bbffc971927c4f0765dc574d659b9a05e2592
Lucas Soler <lucas.p.soler@gmail.com>
2023-11-01 17:54:16 -0300

    banner added at home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { PropertyCarousel } from "@/components/PropertyCarousel";
+import Image from "next/image";
 import { properties } from "./list/page";
+import realtyImage from "/public/realty.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col space-y-20 max-w-screen-2xl m-auto py-8 px-4 md:px-20">
+    <main className="flex min-h-screen flex-col space-y-10 max-w-screen-2xl m-auto py-8 px-4 md:px-20">
+      <div className="flex h-52 md:h-80">
+        <Image
+          src={realtyImage}
+          alt="realty image"
+          className="rounded-xl"
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <div className="flex"></div>
       <div className="flex flex-col space-y-10 h-2/5">
         <h2 className="capitalize font-semibold text-xl">
           featured properties
